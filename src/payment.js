0cb21ffd72cde812b1222970d4ad52b70f262d8c
Pratheek V A <pratheekvaberike@gmail.com>
2022-05-26 22:27:22 +0530

    second commit

--- src/payment.js
+++ src/payment.js
@@ -1,5 +1,5 @@
 export const displayRazorPay = async (name, email, contact, amount) => {
-  const data = await fetch("https://envision2022.herokuapp.com/razorpay", {
+  const data = await fetch("https://envision2.herokuapp.com/razorpay", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
